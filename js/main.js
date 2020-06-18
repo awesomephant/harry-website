605c3f1569eb66b2527bc780c074291dccfc654a
Max Kohler <max@koehler-kn.de>
2020-06-18 12:56:10 +0100

    sketch out touch support

--- js/main.js
+++ js/main.js
@@ -6,18 +6,40 @@ let stack = {
 }
 
 let debug = {}
+let lastTouchY = 0;
 
 function updateDebug() {
     debug.current.textContent = `Current: ${stack.current}`;
 }
 
 function updateStack(offset) {
+    console.log('going to ' + offset)
     let prev = parseInt(stack.el.getAttribute('data-current'))
     let current = prev + offset;
     if (current < stack.total && current > 0) {
         stack.current = current;
         stack.el.setAttribute('data-current', stack.current)
     }
+}
+
+function handleTouchStart(e) {
+    lastTouchY = e.touches[0].clientY;
+}
+function handleTouchEnd(e) {
+    lastTouchY = 0;
+}
+function handleTouchCancel(e) { }
+function handleTouchMove(e) {
+    const touch = e.changedTouches[0];
+    const deltaY = lastTouchY - touch.clientY;
+    let offset = 0;
+    if (deltaY > 0) {
+        offset = 1;
+    } else {
+        offset = -1
+    }
+    updateStack(offset)
+    updateDebug()
 }
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -28,6 +50,11 @@ window.addEventListener('DOMContentLoaded', () => {
     stack.total = stack.items.length;
 
     updateDebug();
+
+    window.addEventListener("touchstart", handleTouchStart, false);
+    window.addEventListener("touchend", handleTouchEnd, false);
+    window.addEventListener("touchcancel", handleTouchCancel, false);
+    window.addEventListener("touchmove", handleTouchMove, false);
 
     window.addEventListener('wheel', e => {
         e.preventDefault()
